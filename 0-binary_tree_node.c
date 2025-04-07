dcfe5fc7acc1aa16fa7c507980ba5d06879e979c
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-07 22:18:18 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,5 @@
 #include"binary_trees.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * binary_tree_node - Entry point
@@ -15,6 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 	new = malloc(sizeof(binary_tree_t));
+
 	if (new == NULL)
 		return (NULL);
 	new->n = value;

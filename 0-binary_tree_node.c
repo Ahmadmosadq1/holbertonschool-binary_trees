798bc98480e81d5bceb2503e24cec4280bc748d1
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-07 22:08:17 +0000

    0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include"binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_node - Entry point
+ *
+ * Description :a funtion that prints a binary tree
+ *
+ * @parent: the parent node.
+ * @value: the value of n
+ * Return: new.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}

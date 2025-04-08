2393c0745549e7d9088d693e68728b5cc90dd417
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-08 02:38:16 +0000

    6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Entery point
+ * decription: does binary tree traversal
+ * @tree: node
+ * @func: function pointer for print_num.
+ * return: void.
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL && func == NULL)
+		return;
+	func(tree->n)
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}

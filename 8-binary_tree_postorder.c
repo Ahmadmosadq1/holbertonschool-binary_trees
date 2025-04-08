93dc06a1c29abde0384b3b989d40d6544cc15f5b
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-08 03:04:19 +0000

    8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Entery point
+ * decription: does binary tree post order
+ * @tree: node
+ * @func: function pointer for print_num.
+ * return: void.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}

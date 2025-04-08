5e7a067da436951e9e4c01387cb6ff660fd54c1a
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-08 02:52:58 +0000

    7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Entery point
+ * decription: does binary tree traversal
+ * @tree: node
+ * @func: function pointer for print_num.
+ * return: void.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+        if (tree == NULL || func == NULL)
+                return;
+        binary_tree_inorder(tree->right, func);
+	func(tree->n);
+        binary_tree_inorder(tree->left, func);
+}

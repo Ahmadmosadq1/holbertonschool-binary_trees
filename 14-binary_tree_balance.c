8b52e22b8dea8f4010605ab17046ebdd29317f1e
SalehFulayyih <saleh.5050@hotmail.com>
2025-04-08 17:13:39 +0000

    14-binary_tree_balance.c and update binary_trees.h

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,14 +29,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+        size_t right_height = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = 0;
-	size_t right_height = 0;
-
 	if (tree->left != NULL)
 	{
 		left_height = 1 + binary_tree_height(tree->left);
